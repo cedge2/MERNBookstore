69046516dfb0422a66e5db12972f7e0c3b5a12af
Camber <cedge2@asu.edu>
2024-09-07 17:26:47 -0500

    Added routes for all books, and route for single book by id

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,36 @@ app.post('/books', async (request, response) => {
     }
 });
 
+//Route for getting all books from database
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({});
+        
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//Route for getting one book from database by id
+app.get('/books/:id', async (request, response) => {
+    try{
+
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+        
+        return response.status(200).json(book);
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
 mongoose    
     .connect(DBURL)
     .then(() => {
